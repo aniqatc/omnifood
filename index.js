f0f3df05f341e90c597dcd22a8a2fafb1c398606
aniqatc <aniqatc97@gmail.com>
2022-12-04 16:04:25 -0500

    Hamburger Menu Function

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 /* HOVER FUNCTIONALITY FOR MOBILE */
 document.addEventListener("touchstart", function () {}, true);
+
+/* HAMBURGER MENU */
+const navButton = document.querySelector(".mobile-nav-btn");
+const headerElement = document.querySelector(".header");
+
+navButton.addEventListener("click", function () {
+	headerElement.classList.toggle("nav-open");
+});
