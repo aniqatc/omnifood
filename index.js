49d9fa4da16f7d59c5fefa29f3e5c10e5bee776b
aniqatc <aniqatc97@gmail.com>
2022-12-05 21:16:19 -0500

    Improve Form Submission Message

--- index.js
+++ index.js
@@ -91,8 +91,9 @@ function submission(event) {
 	let message = document.querySelector(".submission");
 	if (planChoice !== "") {
 		message.style.display = "block";
-		message.textContent = `Thank you for your interest in the ${planChoice} plan, ${name}. Unfortunately, Omnifood is a fictional company and I'm not saving any of this data currently!`;
+		message.textContent = `Thank you for your interest in the ${planChoice} plan, ${name}. Unfortunately, Omnifood is a fictional company so there's no free meal to send you.`;
 	} else {
+		message.style.display = "block";
 		message.textContent = `Sorry, you need to choose a plan to proceed!`;
 	}
 }
